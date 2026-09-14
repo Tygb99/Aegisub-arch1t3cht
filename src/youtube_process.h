3f487b557d684031773c327ca8c32ddfdb3d5689
Tygb99 <245088064+Tygb99@users.noreply.github.com>
2026-09-15 01:08:52 +0900

    Support wxWidgets 3.0 process argument signatures

--- src/youtube_process.h
+++ src/youtube_process.h
@@ -7,8 +7,8 @@
 inline long ExecuteYoutube(wxArrayString const& arguments, int flags, wxProcess *process = nullptr) {
 	std::vector<std::wstring> strings;
 	for (auto const& argument : arguments) strings.push_back(argument.ToStdWstring());
-	std::vector<const wchar_t*> argv;
-	for (auto const& argument : strings) argv.push_back(argument.c_str());
+	std::vector<wchar_t*> argv;
+	for (auto& argument : strings) argv.push_back(argument.data());
 	argv.push_back(nullptr);
 	return wxExecute(argv.data(), flags, process);
 }
